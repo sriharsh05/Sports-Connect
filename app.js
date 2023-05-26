f7c8fa4f7880751533569a04efafc82e1109011d
sriharsh <sriharsh.gampa@outlook.com>
2023-05-26 12:53:09 +0530

    add csrf token

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+var cookieParser = require("cookie-parser");
+var csrf = require("tiny-csrf");
+
+
+app.use(cookieParser("ssh! some secret string"));
+app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+
+
 
 //set ejs as view engine
 app.set("view engine", "ejs");
@@ -7,5 +15,12 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     res.render("index");
 })
+
+app.get("/signup", async (request, response) => {
+    response.render("signup", {
+      title: "Signup",
+      csrfToken: request.csrfToken(),
+    });
+  });
 
 module.exports = app;
