482c9292b8e5fc777aefcc1e9525d9c24a7d8d01
sriharsh <sriharsh.gampa@outlook.com>
2023-05-31 10:29:50 +0530

    add role column in the user table

--- app.js
+++ app.js
@@ -12,15 +12,25 @@ app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 //set ejs as view engine
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render("index");
-})
+
+
+app.get("/", async (request, response) => {
+  response.render("index", {
+    title: "Sports Scheduler",
+    csrfToken: request.csrfToken(),
+  });
+});
 
 app.get("/signup", async (request, response) => {
-    response.render("signup", {
-      title: "Signup",
-      csrfToken: request.csrfToken(),
-    });
+    response.render("signup", {title: "Signup",csrfToken: request.csrfToken(),});
   });
+
+  app.get("/login", (request, response) => {
+    response.render("login", { title: "Login", csrfToken: request.csrfToken() });
+  });
+
+  // app.post("/users", async (request, response) => {
+  //   response.render("admin", { title: "users", csrfToken: request.csrfToken() });
+  // });
 
 module.exports = app;
